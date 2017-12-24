1af6a37cf1855b08c97fc7eecf2be50fc0d7c7c4
nuxf10ir <siiin@inbox.ru>
2017-12-25 01:22:48 +0300

    init
    initial commit

---
+++ js/script.js
@@ -0,0 +1,174 @@
+_.templateSettings = {
+    evaluate: /\{\[([\s\S]+?)\]\}/g,
+    interpolate: /\{\{([\s\S]+?)\}\}/g
+};
+
+var treeParams = {
+    questions: {
+        1: {
+            text: "Кредитный лимит эффективен?",
+            type: "question",
+            buttons: {
+                yes: {
+                    id: 3,
+                    method: "next"
+                },
+                no: {
+                    id: 2,
+                    method: "next"
+                }
+            }
+        },
+        2: {
+            text: "Лимит ввели раньше 3 месяцев до конца года",
+            type: "question",
+            buttons: {
+                yes: {
+                    id: 6,
+                    method: "end"
+                },
+                no: {
+                    id: 3,
+                    method: "next"
+                }
+            }
+        },
+        3: {
+            text: "Лимит востребован клиентами группы?",
+            type: "question",
+            buttons: {
+                yes: {
+                    id: 4,
+                    method: "next"
+                },
+                no: {
+                    id: 5,
+                    method: "end"
+                }
+            }
+        },
+        4: {
+            text: "Доля просроченной дебиторки в общей задолженности растет?",
+            type: "question",
+            buttons: {
+                yes: {
+                    id: 7,
+                    method: "end"
+                },
+                no: {
+                    id: 8,
+                    method: "end"
+                }
+            }
+        },
+        5: {
+            text: "Доля просроченной дебиторки в общей задолженности растет?",
+            type: "question",
+            buttons: {
+                yes: {
+                    id: 9,
+                    method: "end"
+                },
+                no: {
+                    id: 10,
+                    method: "next"
+                }
+            }
+        },
+        6: {
+            type: "answer",
+            text: "Переведите клиентов на систему поставок по факту оплаты. Начните с компаний с наименьшей эффективностью и стратегической ценностью для организации",
+            buttons: null
+        },
+        7: {
+            type: "answer",
+            text: "Увеличьте кредитный лимит до уровня фактической потребности клиента или с запасом с учетом динамики продаж. Период отсрочки корректируйте исходя из анализа эффективности: если уровень эффективности на прежней отметке, переведите часть клиентов с большей долей просрочки на меньший срок оплаты. Если эффективность выше средней, увеличьте рассрочку.",
+            buttons: null
+        },
+        8: {
+            type: "answer",
+            text: "Увеличьте кредитный лимит до уровня потребности клиента или с запасом с учетом динамики продаж. Период отсрочки оставьте без изменений.",
+            buttons: null
+        },
+        9: {
+            type: "answer",
+            text: "Сократите кредитный лимит до уровня фактической потребности клиента. Период отсрочки коррктируйте исходя из анализа эффективности: если уровень эффективности на нижней отметке, переведите часть клиентов с большей долей просрочки на меньший срок оплаты. Если эффективность выше средней, увеличьте рассрочку.",
+            buttons: null
+        },
+        10: {
+            type: "answer",
+            text: "Сократите кредитный лимит до уровня фактической потребности клиента. Период отсрочки оставьте без изменений.",
+            buttons: null
+        }
+
+
+
+    }
+};
+
+jQuery.fn.questionsTree = function(steps) {
+    var $self = $(this),
+        cardTmpl = _.template($("#card-tmpl").html());
+
+
+    function startCard(id) {
+        var $card = $(cardTmpl(steps.questions[id])),
+            $buttons = $card.find(".card-button");
+
+        $buttons.length && $buttons
+            .bind("click.answer", function () {
+                var $this = $(this),
+                   data = $this.data();
+
+                if (data.method === "reset") {
+
+                    clearTree();
+
+                } else {
+                    $buttons
+                        .parent(".card-button_out")
+                        .removeClass("selected");
+
+                    $this
+                        .parent(".card-button_out")
+                        .addClass("selected");
+
+                    $this
+                        .parents(".card")
+                        .nextAll()
+                        .remove();
+
+                    startCard(data.id);
+                }
+
+
+
+            });
+
+        $card.appendTo($self);
+
+        $(window).scrollTo($card[0], 400);
+
+    }
+
+    function clearTree() {
+        $self.empty();
+        startCard(1);
+    }
+
+
+
+    return {
+        next: function(id) {
+            startCard(id);
+        },
+        reset: function () {
+            clearTree()
+        }
+    };
+};
+
+
+$("#cards-tree").questionsTree(treeParams).next(1);
+
+
